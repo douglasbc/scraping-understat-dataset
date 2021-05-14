71d6925bab0932ac60d776870c097f0e429ef46f
Douglas Carrion <douglasbcarrion@gmail.com>
2021-05-14 13:41:41 -0300

    updated 13/05 matches

--- scraping/update.py
+++ scraping/update.py
@@ -2,5 +2,5 @@ import scraping.match_scraper as ms
 import scraping.season_scraper as ss
 
 # Updates the shots datasets and the players datasets
-ms.update_shots_dataset(2020)
+#ms.update_shots_dataset(2020)
 ss.generate_players_csv(2020,2020)
